db402617be2bf648f319c940b15b1b4ee6b1c7c9
emilyzhou0706@gmail.com <peter@newstonespheretech.com>
2021-06-22 13:10:13 +1000

    try to push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+//   return (
+//     <>
+//       <Router>
+//         <Navbar />
+//         <Switch>
+//           <Route path='/' exact />
+//         </Switch>
+//       </Router>
+//     </>
+//   );
+// }
+class App extends React.Component {
+  state = {
+    firstName:'',
+    age:'',
+    email:''
+  }
+
+  render() {
+    const {firstName,age,email} = this.state
+    let emailInput;
+    if(age >= 14) {
+      emailInput = <input name={email}/>
+    }
+    
+    return (
+    <div>
+     <input name={firstName}/>
+     <input name={age}/>
+     {emailInput}
     </div>
-  );
+    )
+  }
 }
+
+document.body.innerHTML = "<div id='root'></div>";
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
 
 export default App;
